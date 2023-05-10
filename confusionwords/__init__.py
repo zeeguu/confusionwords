3ded49724f9de29808eb28565cf73b6f46874ca7
Mircea Lungu <mircea.lungu@gmail.com>
2023-05-10 23:46:38 +0300

    elaborated a bit on the example

--- confusionwords/__init__.py
+++ confusionwords/__init__.py
@@ -12,8 +12,9 @@ ConfusionSets = {
 # Example of getting and parsing the data in a json file found in the data folder for the package
 # the documentation claims that "The resource argument should be in the form of a relative filename, using / as the path separator"
 # so in principle you should not need to use the os.path.join anymore
-data = pkgutil.get_data(__name__, "data/da/dk_small_sub_sample.json")
-print(json.loads(data))
+raw_bytes = pkgutil.get_data(__name__, "data/da/dk_small_sub_sample.json")
+dict = json.loads(raw_bytes.decode('utf-8'))
+print(dict.keys())
 
 # exiting here because you'll have to redo the load_confusionset by using the same pattern as above
 
