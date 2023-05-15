83ef7684d36c581846e1c524c25c24329151a1f4
Tiago Ribeiro <tfnribeiro@gmail.com>
2023-05-15 09:10:24 +0200

    Moved the confusion set to be part of the object class.
    - This means we can specify a specific LEMMA dictionary according to languages, as it might make sense to not store forms for certain POS. For example, English doesn't have different forms of determinants. This can be implemented as cases (as it is now), or by providing a dictionary for specific languages.
    - Import the DEFAULT_LEMMA_CONFUSION_SET as part of the init so it can be checked in other parts of the code.

--- confusionwords/__init__.py
+++ confusionwords/__init__.py
@@ -1,5 +1,4 @@
-from .confusion_set import ConfusionSet
-import os
+from .confusion_set import ConfusionSet, DEFAULT_LEMMA_CONFUSION_SET
 import pkgutil
 import json
 
@@ -12,6 +11,7 @@ ConfusionSets = {
 # Example of getting and parsing the data in a json file found in the data folder for the package
 # the documentation claims that "The resource argument should be in the form of a relative filename, using / as the path separator"
 # so in principle you should not need to use the os.path.join anymore
+
 raw_bytes = pkgutil.get_data(__name__, "data/da/da_small_confusion_set.json")
 dict = json.loads(raw_bytes.decode('utf-8'))
 ConfusionSets["da"].load_confusionset_from_dict(dict)
